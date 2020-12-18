f5348fff44ac2bc9e78247101ab391aeff2686f8
uyghur-io <admin@uyghur.io>
2020-12-18 18:00:11 +0300

    -

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,28 +1,31 @@
-import React from 'react'
-import { Card, Form, Button } from 'react-bootstarp'
+import React, { useRaf } from 'react'
+import { Card, Form, Button } from 'react-bootstrap'
 
 export default function Signup() {
+  const emailRaf = useRaf
+  const passwordRaf = useRaf
+  const passwordConfirmRaf = useRaf
     return (
         <>
           <Card>
-              <Card.bady>
+              <Card.Body>
                   <h2 className="text-center md-4">Sign Up</h2>
                   <Form>
                       <Form.Group id="email">
                         <Form.Lable>Email</Form.Lable>
-                        <Foem.Control type="email" ref={emailRaf} required/>
+                        <Form.Control type="email" ref={emailRaf} required/>
                       </Form.Group>
                       <Form.Group id="password">
                         <Form.Lable>Password</Form.Lable>
-                        <Foem.Control type="password" ref={passwordRaf} required/>
+                        <Form.Control type="password" ref={passwordRaf} required/>
                       </Form.Group>
                       <Form.Group id="password-confirm">
                         <Form.Lable>Password Confirmation</Form.Lable>
-                        <Foem.Control type="password" ref={passwordConfirmRaf} required/>
+                        <Form.Control type="password" ref={passwordConfirmRaf} required/>
                       </Form.Group>
                       <Button className="w-100" type="submit">Sign Up</Button>
                   </Form>
-              </Card.bady>
+              </Card.Body>
           </Card>
           <div className="w-100 text-center mt-2">
               Already have an account? Log In 
